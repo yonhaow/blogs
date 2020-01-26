3864053362cde58debd4827e87b4b73b60f0d683
yonhaow <yonhaow@163.com>
2020-01-26 21:54:19 +0800

    fix(fix git page url error.): 修复 Git Page File Url 错误

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,7 +15,7 @@ module.exports = {
     // "@vuepress/nprogress"
   ],
   // 设置站点根路径，如果你在访问的地址是 'www.xxxx.com/wxDocs' 那么就设置成 '/wxDocs/'
-  base: "/blogs",
+  base: "/blogs/",
   // 添加 github 链接，但是这个要放在公司的内网服务器，所以为空
   // repo: 'https://yonhaow.gitee.io/blogs/'
   // 网站语言
